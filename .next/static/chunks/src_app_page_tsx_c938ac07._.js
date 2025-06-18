c5e557cb6fd6f4f1f28f22536d64b9f8f0024a2f
zeir1x <luis.sanchez.2.g@gmail.com>
2025-06-18 13:51:31 -0600

    Add .next/static/chunks/src_app_page_tsx_c938ac07._.js 1750276291226

---
+++ .next/static/chunks/src_app_page_tsx_c938ac07._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_4ec79d8f._.js",
+  "static/chunks/src_e2665401._.js"
+],
+    source: "dynamic"
+});
